c396e69f9e9f5732e45dbf4b14d19270727f6975
Patrick Bär <pbaer@hof-university.de>
2020-05-20 13:52:49 +0000

    added solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,34 @@
-pub struct NumArray {}
+pub struct NumArray {
+    numbers: Vec<i32>
+}
 
 impl NumArray {
-    pub fn new(numbers: Vec<i32>) -> Self {
-        todo!()
+    pub fn new(mut numbers: Vec<i32>) -> Self {
+        let mut sum = 0;
+        for n in numbers.iter_mut() {
+            sum += *n;
+            *n = sum;
+        }
+        NumArray { numbers: numbers }
     }
 
     pub fn sum_range(&self, i: i32, j: i32) -> i32 {
-        todo!()
+        let top = self.numbers[j as usize];
+        /*let bottom = match i {    //functional programming
+            0 => 0,
+            _ => self.numbers[i as usize - 1]
+        };*/
+        let bottom = if i == 0 {
+            0
+        } else {
+            self.numbers[i as usize - 1]
+        };
+        top - bottom
+        
+        /*let mut sum = 0;
+        for index in i..=j {
+            sum += self.numbers[index as usize]
+        }
+        sum*/
     }
 }
